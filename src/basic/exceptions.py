377ae0cf4fa647fbdcaf5e075bf17d5c33379967
SyYhunfhds <SY1438439330@163.com>
2025-07-21 12:36:27 +0800

    [util]: 编写完成查询接口模块

--- src/basic/exceptions.py
+++ src/basic/exceptions.py
@@ -1,46 +1,53 @@
-# 导入自定义模块
 from etc import _
+
+# 总封装
+class FofaException(Exception):
+    def __init__(self, message: str = "Fofa Exception", *args, **kwargs):
+        super().__init__(_(message), *args, **kwargs)
 
 class FofaAPIException(Exception): # API 相关异常
     def __init__(self, message: str = "Fofa API Exception", *args, **kwargs):
-        super().__init__(_(message), *args, **kwargs)
+        super().__init__(message, *args, **kwargs)
         
 class FofaQueryException(Exception): # 查询相关异常
     def __init__(self, message: str = "Fofa Query Exception", *args, **kwargs):
-        super().__init__(_(message), *args, **kwargs)
+        super().__init__(message, *args, **kwargs)
         
 class FofaUtilException(Exception): # 工具相关异常
     def __init__(self, message: str = "Fofa Utility tools Exception", *args, **kwargs):
-        super().__init__(_(message), *args, **kwargs)
+        super().__init__(message, *args, **kwargs)
         
 # 具体的异常类在此
 # API配置相关异常        
 class EmptyKeyError(FofaAPIException):
     def __init__(self, message: str = "The API key is empty. Please check the configuration", *args, **kwargs):
-        super().__init__(_(message), *args, **kwargs)
+        super().__init__(message, *args, **kwargs)
 class NonOfficialKeyWarning(FofaAPIException):
     def __init__(self, message: str = "Using an unofficial key may prevent \
         some special interfaces from functioning properly", *args, **kwargs):
-        super().__init__(_(message), *args, **kwargs)
+        super().__init__(message, *args, **kwargs)
 
 # 查询相关异常
 class LowCreditWarning(FofaQueryException):
     def __init__(self, message: str = "The available credit of the API \
         is too low. Please pay attention to the remaining balance", *args, **kwargs):
-        super().__init__(_(message), *args, **kwargs)
+        super().__init__(message, *args, **kwargs)
 class ZeroCreditWarning(FofaQueryException):
     def __init__(self, message: str = "The available credit of the API \
         has run out. Please recharge immediately", *args, **kwargs):
-        super().__init__(_(message), *args, **kwargs)
+        super().__init__(message, *args, **kwargs)
 class EmptyResultsWarning(FofaQueryException):
     def __init__(self, message: str = "No results were found for this query", *args, **kwargs):
-        super().__init__(_(message), *args, **kwargs)
-class ConnectionError(FofaQueryException):
+        super().__init__(message, *args, **kwargs)
+class FofaConnectionError(FofaQueryException):
     def __init__(self, message: str = "Connection error occurred during query execution", *args, **kwargs):
-        super().__init__(_(message), *args, **kwargs)
-class SyntaxError(FofaQueryException):
+        super().__init__(message, *args, **kwargs)
+class FofaRequestFailed(FofaQueryException):
+    def __init__(self, message: str = "Request failed", *args, **kwargs):
+        super().__init__(message, *args, **kwargs)
+class FofaQuerySyntaxError(FofaQueryException):
     def __init__(self, message: str = "Syntax error in query string", *args, **kwargs):
-        super().__init__(_(message), *args, **kwargs)
+        super().__init__(message, *args, **kwargs)
         
 # 工具相关异常
 # 暂时置空
--- src/util/core.py
+++ src/util/core.py
@@ -1,5 +1,6 @@
 # 导入第三方依赖
 import requests
+from requests import ConnectionError, ConnectTimeout
 from loguru import logger
 
 # 导入标准库
@@ -10,11 +11,20 @@ from base64 import b64encode
 from ..basic import *
 
 def query(
-    logger, translator, url: str, apikey: str, query_string: str, 
-    headers: dict = {}, cookies: dict = {},
-    size: int = 10000, page: int = 1, fields: list[str] = [
+    logger, # 日志记录器
+    translator, # gettext国际化接口
+    url: str, # fofa查询接口(为了兼容不同接口和不同API)
+    apikey: str, # fofa密钥
+    query_string: str, # fofa查询字符串, 要没有base64编码的原始文本
+    headers: dict = {}, # 自定义请求头
+    cookies: dict = {}, # cookies
+    size: int = 10000, # 单次最大返回条数
+    page: int = 1, # 查询分页参数
+    fields: list[str] = [
         'title', 'host', 'link', 'os', 'server', 'icp', 'cert'
-    ], full: bool = False
+    ], # 返回字段
+    full: bool = False, # 是否查询所有数据
+    threshold_remaining_queries: int = 1 # 剩余查询次数阈值
 ):
     _ = translator # 换个名称
     params = {
@@ -25,7 +35,34 @@ def query(
         'size': size,
         'page': page
     }
-    result = requests.get(url, params=params, headers=headers, cookies=cookies)
-
+    try:
+        result = requests.get(url, params=params, headers=headers, cookies=cookies)
+    except (ConnectionError, ConnectTimeout) as e:
+        logger.error(_("Connection error or timeout"))
+        raise FofaConnectionError(_("Connection error or timeout"))
     
-    pass
+    if result.status_code != 200:
+        logger.error(_("Request failed with status code {}").format(result.status_code))
+        raise FofaRequestFailed(_("Request failed with status code {}").format(result.status_code))
+    result = result.json()
+    if result['error']:
+        logger.error(_("Request failed with error message {}").format(result['errmsg']))
+        if '820000' in result['errmsg']:
+            raise FofaQuerySyntaxError()
+        else:
+            raise FofaRequestFailed(_("Request failed with error message {}").format(result['errmsg']))
+    
+    if result['remaining_queries'] == threshold_remaining_queries:
+        msg = "The available credit of the API has been exhausted, \
+            and further queries cannot be made"
+        logger.warning(_(msg))
+        raise LowCreditWarning(msg)
+    elif result['remaining_queries'] == 0:
+        logger.warning(_(msg))
+        raise ZeroCreditWarning(msg)
+    
+    if result['size'] == 0:
+        logger.warning(_(msg))
+        raise EmptyResultsWarning(msg)
+    
+    return result
