f4bf085b649fc589aa71cb71c88dd7acfc6917f4
SyYhunfhds <SY1438439330@163.com>
2025-07-23 13:56:04 +0800

    V0.9.1
    feat: 官方和第三方查询接口基本测试完毕
    misc: 更新main.py，添加示例代码

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from python-fofa-sy!")
+from .src import Fofa
 
+def main():
+    query_string = "domain=\"example.com\""
+    offical_api = "https://fofa.info"
+    apikey = "xxxxxxxxxxxx"
+    fofa = Fofa(api=offical_api, key=apikey)
+    print(fofa._search_url)
+    res = fofa.search(
+        query_string=query_string,
+        size=100
+    )
+    print(fofa.fields)
+    fofa - 'unk1' # 删除一些列
+    fofa - 'unk2'
+    fofa - 'unk3' 
+    print(fofa.assets)
+    print(fofa.fields)
+    print(fofa.domain) # 获取domain list
+    print(type(fofa.domain))
+    fofa - 'new_empty_col' # 可以增加一个空列
+    print(len(fofa)) # 获取结果数量
 
 if __name__ == "__main__":
     main()
