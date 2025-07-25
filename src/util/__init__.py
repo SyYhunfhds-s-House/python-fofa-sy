2f27de3228bda33419926a0f6c6932ec102a2854
SyYhunfhds <SY1438439330@163.com>
2025-07-25 16:49:09 +0800

    V1.0.1
    将源码目录移入src/fofa_py中以精简打包结构
    将依赖的tablib[all]改回tablib以精简项目依赖；现在如果有额外导入导出需求的请自行安装tablib[all]

--- src/util/__init__.py
+++
@@ -1,8 +0,0 @@
-from .query import search, stats, host
-from .query import search_v2, stats_v2, host_v2
-
-__all__ = [
-    'search', 'search_v2',
-    'stats', 'stats_v2',
-    'host', 'host_v2',
-]
